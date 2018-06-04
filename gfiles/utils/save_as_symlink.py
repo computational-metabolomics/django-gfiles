bb7c9a7861d0311a0ba1857c6cb0055965ac71e5
tomnl <thomas.nigel.lawson@gmail.com>
2018-06-04 19:34:28 +0100

    symlink

---
+++ gfiles/utils/save_as_symlink.py
@@ -0,0 +1,19 @@
+import os
+import tempfile
+from django.core.files import File
+from django.conf import settings
+
+def save_as_symlink(abs_pth, name, file_data_obj):
+    tf = tempfile.NamedTemporaryFile(delete=False)
+
+    # first create link to empty file
+    file_data_obj.data_file.save(name, File(open(tf.name)))
+
+    fpth = os.path.join(settings.MEDIA_ROOT, file_data_obj.data_file.name)
+    print 'file path', fpth
+    os.remove(fpth)
+    os.symlink(abs_pth, fpth)
+    file_data_obj.save()
+    tf.close()
+
+    return file_data_obj
