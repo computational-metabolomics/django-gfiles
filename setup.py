f1c319424243d4cb21b9f43b87fa0441ff0163fc
Tomnl <thomas.nigel.lawson@gmail.com>
2018-07-15 13:58:01 +0100

    travis pypi troublshoot2

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+
 setup(
     name='django-gfiles',
     version='0.0.2',
