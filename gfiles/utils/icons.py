3abd71af36d0309c29530150d9e7caa9f889d87a
Tomnl <thomas.nigel.lawson@gmail.com>
2018-08-27 13:26:37 +0100

    add icon marksafe utils

---
+++ gfiles/utils/icons.py
@@ -0,0 +1,6 @@
+from django.utils.safestring import mark_safe
+
+EYE = mark_safe('<i class="fa fa-eye" aria-hidden="true"></i>')
+DOWN = mark_safe('<i class="fa fa-fw fa-download"></i>')
+PLAY = mark_safe('<i class="fa fa-fw fa-play"></i>')
+SAVE = mark_safe('<i class="fa fa-fw fa-save"></i>')
